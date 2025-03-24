f398d87e72166c1a26d3e81fd1da20d7eee6a237
edwin-magairwa <edwinmagairwa@gmail.com>
2025-03-24 21:36:31 +0300

    added arrays

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+// The initial cats array is defined in the test file and reset before each test
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Appends a cat to the end of the array (non-destructive)
+function appendCat(name) {
+    return [...cats, name];
+}
+
+// Prepends a cat to the beginning of the array (non-destructive)
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+// Removes the last cat from the array (non-destructive)
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+// Removes the first cat from the array (non-destructive)
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+// Appends a cat to the end of the array (destructive)
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Prepends a cat to the beginning of the array (destructive)
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+// Removes the last cat from the array (destructive)
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+// Removes the first cat from the array (destructive)
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
